25aa052e719f23c7fba6933eb64900cf11a6161b
jsanabrh04 <juanpablosanabriahoyos@gmail.com>
2024-09-18 13:19:23 -0500

    Basic documentation is created using Swagger to test the application's endpoints.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +26,14 @@ async function bootstrap() {
   });
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  const config = new DocumentBuilder()
+    .setTitle('HOUSY HOST TECHNICAL TEST')
+    .setDescription(``)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(5432);
 
